9ba4480e2033959eae9d5e2f8d2949e2e9a5a8d3
marcjacksonuktv <60617318+marcjacksonuktv@users.noreply.github.com>
2021-10-20 13:10:38 +0100

    Add files via upload

---
+++ scraps 3.py
@@ -0,0 +1,44 @@
+import sys
+import time
+from time import gmtime, strftime
+import datetime
+import conf as conf
+import self as self
+now = datetime.datetime.now()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+options = Options()
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+testurl = 'https://uktv:wemakegreattv@uktvplay.uatuktv.co.uk/'
+a = ActionChains(driver)
+driver.get(testurl)
+driver.implicitly_wait(10)
+
+# Cookie Notice
+CookieYes = driver.find_element_by_id('cookie-agree').click()
+
+# Header
+uktvplaylogo = driver.find_element_by_id('nav-bar-home')
+categories = driver.find_element_by_id('nav-bar-categories')
+channels = driver.find_element_by_id('nav-bar-channels')
+boxsets = driver.find_element_by_id('nav-bar-boxsets')
+a2z = driver.find_element_by_id('nav-bar-az')
+more = driver.find_element_by_id('nav-bar-more')
+mylist = driver.find_element_by_id('nav-bar-mylist')
+account = driver.find_element_by_id('nav-bar-account')
+search = driver.find_element_by_id('nav-bar-search')
+
+# Sign In
+account.click()
+email = driver.find_element_by_id('email').send_keys('chromeuser@testaccount.com')
+password = driver.find_element_by_id('password').send_keys('password123')
+SignIn_button = driver.find_element_by_id('sign-in-btn').click()
+time.sleep(3)
